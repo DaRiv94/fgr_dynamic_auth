d4366a2bf6c04fb3712016384fbebedc8da9873d
Frankie Riviera <frankgriviera@outlook.com>
2020-07-26 18:58:06 -0400

    added startup_data logic

--- databases/mongodb/startup.js
+++ databases/mongodb/startup.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 config = require('../../config');
 
+
 //https://docs.mongodb.com/manual/tutorial/query-documents/
 //https://docs.mongodb.com/compass/master/query/filter/
 module.exports=()=>{
@@ -13,15 +14,14 @@ if (config.database_connectionstring_dev && process.env.NODE_ENV=='development')
      db = config.database_connectionstring_dev
 }
 
-mongoose.connect(db, { useNewUrlParser: true })
-.then(()=> console.log(`Connecting to ${config.database_type} database...`));
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> {
+     console.log(`Connecting to ${config.database_type} database...`);
+     
+     //If connection is successful, then create default start up data if specified in configuration.
+     require("./startup_data")();
+
+     });
 }
 
-if(config.default_user_email && default_user_password){
-     console.log("Default user and default user password exist")
-}
-//if default users exist then create default users if they do not already exists
-//check if default user and default admin exist in config
-//if they do check db to see if they already exist
-//if they do already exist, update their passwords with config passwords
-//if they do not exist then create the default users in the db
