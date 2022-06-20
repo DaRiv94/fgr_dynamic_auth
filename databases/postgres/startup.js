9e51258be15bdd8408c9dffbf067850fc684792d
Frankie Riviera <frankgriviera@outlook.com>
2022-06-20 05:51:50 -0400

    updated db logic, updated dockerfile with expose cmd

--- databases/postgres/startup.js
+++ databases/postgres/startup.js
@@ -5,26 +5,59 @@ config = require('../../read_config');
 connection_string = config.database_connectionstring
 
 //If there is a mongodb_dev and its development mode then use that db
-if (config.database_connectionstring_dev && process.env.NODE_ENV=='development'){
+if (config.database_connectionstring_dev && process.env.NODE_ENV == 'development') {
     connection_string = config.database_connectionstring_dev
 }
 
 
 
 
+// const db = new Sequelize(connection_string, {
+//     dialect: 'postgres',
+//     ssl: true,
+//     dialectOptions: {
+//         ssl: {
+//             require: true
+//         }
+//     }
+// }) ? process.env.NODE_ENV == 'production'
+//     : new Sequelize(connection_string, {
+//         dialect: 'postgres'
+//     })
 
-//Postgres DB
-const db = new Sequelize(connection_string, {
-    dialect: 'postgres',
-    ssl:true,
-        dialectOptions:{
-           ssl:{
-              require:true
-           }
+// const db = new Sequelize(connection_string, {
+//     dialect: 'postgres'
+// }) 
+
+// const db = new Sequelize(connection_string, {
+//     dialect: 'postgres',
+//     ssl: true,
+//     dialectOptions: {
+//         ssl: {
+//             require: true
+//         }
+//     }
+// }) 
+
+const db = process.env.NODE_ENV == 'development' ?
+    new Sequelize(connection_string, {
+        dialect: 'postgres'
+    }) : new Sequelize(connection_string, {
+        dialect: 'postgres',
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
         }
-  })
+    })
 
-  //The SSL may be on connnection string seeing as ?sslmode=require I might add logic later to add ssl above based on this in connection string or not
+
+
+//The SSL may be on connnection string seeing as ?sslmode=require I might add logic later to add ssl above based on this in connection string or not
+
+
+
 
 
 db
